c3415103ffe0e61fd13ae22c1d116d73bb7cd032
KatyaKalache <katya@kalache.fr>
2018-12-14 04:04:19 +0000

    styling

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -6,8 +6,8 @@ def add_arrays(arr1, arr2):
     if len(arr1) == len(arr2):
         i = 0
         while i <= len(arr1) - 1:
-            res.append(arr1[i]+arr2[i])
+            res.append(arr1[i] + arr2[i])
             i += 1
-        return (res)
+        return res
     else:
-        return (None)
+        return None
