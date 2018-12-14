1694e51165a37ca4ede652d87f579a96432bc1e8
KatyaKalache <katya@kalache.fr>
2018-12-14 05:53:51 +0000

    adding doc strings

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
+"""
+adds two matrices element-wise
+"""
+
+
 def add_matrices2D(mat1, mat2):
+    """
+    Checking length of matricies and its elements
+    Appending one to another
+    Return common array
+    """
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
     res = []
