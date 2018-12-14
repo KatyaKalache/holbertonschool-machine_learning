a477235aff23e35a859f8ee7215043aef7e04ee3
KatyaKalache <katya@kalache.fr>
2018-12-14 05:58:17 +0000

    adding doc strings

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+"""
+transpose of a 2D matrix
+"""
 
 
 def matrix_transpose(matrix):
+    """
+    Transposing matriecies by flipping rows with columns
+    Return transposed matrix as a new 2d array
+    """
     res = [[matrix[j][i] for j in range(len(matrix))] for i in
            range(len(matrix[0]))]
     return (res)
