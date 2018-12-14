e8149ba516b406271edae1675e91ae3e6b3c242e
KatyaKalache <katya@kalache.fr>
2018-12-14 05:04:22 +0000

    initial

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# adds two matrices element-wise
+
+def add_matrices2D(mat1, mat2):
+  if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+    return None
+  res = []
+  for i in range(len(mat1)):
+    row = []
+    for j in range(len(mat1[0])):
+      row.append(mat1[i][j] + mat2[i][j])
+    res.append(row)
+  return (res)
