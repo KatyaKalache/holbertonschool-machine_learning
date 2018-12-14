42ea7c7b702af820c0f49305736a31ba5464cf02
KatyaKalache <katya@kalache.fr>
2018-12-14 05:56:02 +0000

    adding doc strings

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
+"""
+adds two arrays element-wise
+"""
+
+
 def add_arrays(arr1, arr2):
+    """
+    Checking if both arrays are the same shape
+    If so summing up their elements
+    Return res array as a sum of 2
+    """
     if arr1 is None or arr2 is None:
         return None
     res = []
