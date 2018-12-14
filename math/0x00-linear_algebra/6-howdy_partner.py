9cdc973e1e46d931d706cea3014caee0fb49a422
KatyaKalache <katya@kalache.fr>
2018-12-14 05:39:27 +0000

    rm top comment

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-# concatenates two arrays
 
 
 def cat_arrays(arr1, arr2):
-    """Appending one array to another"""
+    """
+    Appending one array to another
+    """
     res = []
     for i in arr1:
         res.append(i)
