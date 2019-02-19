c7b5ca3c757446bbf370ebbb36bbb8a00050a983
KatyaKalache <katya@kalache.fr>
2019-02-19 20:54:23 +0000

    Concatenating numpy arrays at a specific axis

---
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import numpy as np
+"""
+Concatenates numpy arrays at a specific axis
+"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """
+    Performing concatenation using numpy
+    """
+    res = np.concatenate((mat1, mat2), axis)
+    return (res)
