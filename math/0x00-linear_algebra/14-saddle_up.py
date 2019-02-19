1e5f4be4eda1ef92d4a5c0f4def8fc4c1f3e8824
KatyaKalache <katya@kalache.fr>
2019-02-19 21:20:00 +0000

    Performing multuplication of numpy arrays

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import numpy as np
+"""
+Multyplie elements of 2 arrays
+"""
+
+
+def np_matmul(mat1, mat2):
+    """
+    Using numpy.matmul to perform the operation
+    """
+    mul = np.matmul(mat1, mat2)
+    return (mul)
