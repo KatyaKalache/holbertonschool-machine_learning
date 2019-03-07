12f5abe5cbb6feaa0bb4dc31c931de74fc42f944
KatyaKalache <katya@kalache.fr>
2019-03-07 07:18:53 +0000

    using list comprih , rm numpy import

--- math/0x01-calculus/9-sum_total.py
+++ math/0x01-calculus/9-sum_total.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import numpy as np
 """
 Returns a sum of power 2 digits
 """
@@ -10,8 +9,7 @@ def summation_i_squared(n):
     Calculating the sum of power to 2
     """
     if isinstance(n, (float, int)) and n > 0:
-        arr = np.power(np.arange(1, n+1, 1), 2)
-        res = np.sum(arr)
-        return (int(res))
+        res = [i**2 for i in range(n+1)]
+        return (int(sum(res)))
     else:
         return None
