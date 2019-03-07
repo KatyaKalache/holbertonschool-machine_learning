bcee5edb5564dc0563181da4280ef475ada35df9
KatyaKalache <katya@kalache.fr>
2019-03-07 05:10:48 +0000

    Sum of power 2 of each numpy array digit

---
+++ math/0x01-calculus/9-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+sum_of_power = __import__('9-sum_total').summation_i_squared
+
+print(sum_of_power(5))
---
+++ math/0x01-calculus/9-sum_total.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import numpy as np
+"""
+Returns a sum of power 2 digits
+"""
+
+
+def summation_i_squared(n):
+    """
+    Calculating the sum of power to 2
+    """
+    try:
+        isinstance(n, (int, float))
+        arr = np.power(np.arange(1, n, 1), 2)
+        res = np.sum(arr)
+        return (res)
+    except ValueError:
+        return None
