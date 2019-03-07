0b4b394cb1d29f2820150bef2a5f67e93cc4a575
KatyaKalache <katya@kalache.fr>
2019-03-07 06:25:09 +0000

    Checking if n is positive, switching to if condition

--- math/0x01-calculus/9-sum_total.py
+++ math/0x01-calculus/9-sum_total.py
@@ -9,10 +9,9 @@ def summation_i_squared(n):
     """
     Calculating the sum of power to 2
     """
-    try:
-        isinstance(n, (int, float))
+    if isinstance(n, (float, int)) and n > 0:
         arr = np.power(np.arange(1, n+1, 1), 2)
         res = np.sum(arr)
         return (int(res))
-    except ValueError:
+    else:
         return None
