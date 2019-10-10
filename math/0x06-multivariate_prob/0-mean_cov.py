4cb2b564f46976d7bd1ddfb6a6a2f111447d8341
KatyaKalache <katya@kalache.fr>
2019-10-10 01:46:22 +0000

    Calculating mean and covarience of X

---
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Calculates the mean and covariance of a data set
+"""
+import numpy as np
+
+def mean_cov(X):
+    """
+    Returns: mean, cov
+    """
+    if X.shape[1] < 2:
+        raise ValueError("X must contain multiple data points")
+    if len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    mean = X.mean(axis=0)
+    X -= mean
+    cov = np.dot(X.T, X) / len(X)
+    return mean, cov
