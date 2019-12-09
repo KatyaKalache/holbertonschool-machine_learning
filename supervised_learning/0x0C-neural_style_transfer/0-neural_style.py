38fe28bfba8dc29a5f48a799841bcd83b29d393b
KatyaKalache <katya@kalache.fr>
2019-12-09 19:31:56 +0000

    Error checks for alpha, beta, array type and shape

--- supervised_learning/0x0C-neural_style_transfer/0-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/0-neural_style.py
@@ -19,6 +19,17 @@ class NST:
         """
         Class constructor
         """
+        if type(style_image) is not np.ndarray or len(style_image.shape) != 3:
+            raise TypeError(
+                "style_image must be a numpy.ndarray with shape (h, w, 3)")
+        if type(content_image) is not \
+           np.ndarray or len(content_image.shape) != 3:
+            raise TypeError(
+                "content_image must be a numpy.ndarray with shape (h, w, 3)")
+        if alpha < 0:
+            raise TypeError("alpha must be a non-negative number")
+        if beta < 0:
+            raise TypeError("beta must be a non-negative number")
         self.style_image = self.scale_image(style_image)
         self.content_image = self.scale_image(content_image)
         self.alpha = 10000
@@ -36,5 +47,6 @@ class NST:
         new_height = int(new_width * height / width)
         image_scaled = tf.image.resize_images(image, (new_height, new_width))
         image_scaled = np.resize(image_scaled, (1, new_height, new_width, 3))
-        image_scaled = (image_scaled - np.min(image_scaled))/np.ptp(image_scaled)
+        image_scaled = (
+            image_scaled - np.min(image_scaled))/np.ptp(image_scaled)
         return image_scaled
