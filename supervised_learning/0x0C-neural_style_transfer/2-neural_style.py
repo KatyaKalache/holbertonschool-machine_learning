9567fc20ac7d3e63b289a1be8bbfc54fa71b9fa3
KatyaKalache <katya@kalache.fr>
2019-12-31 06:11:31 +0000

    Repushing with new load model and resize methods

--- supervised_learning/0x0C-neural_style_transfer/2-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/2-neural_style.py
@@ -11,8 +11,8 @@ class NST:
     """
     Class NST
     """
-    style_layers = ['block1_conv1', 'block2_conv1',
-                    'block3_conv1', 'block4_conv1', 'block5_conv1']
+    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
+                    'block4_conv1', 'block5_conv1']
     content_layer = ['block5_conv2']
 
     def __init__(self, style_image, content_image, alpha=1e1, beta=1e3):
@@ -48,17 +48,23 @@ class NST:
         """
         Creates the model used to calculate cost
         """
-        vgg = tf.keras.applications.vgg19.VGG19()
+        vgg = tf.keras.applications.vgg19.VGG19(include_top=False)
+        avg = tf.keras.Sequential()
         for layer in vgg.layers:
             layer.trainable = False
-        style_outputs = [vgg.get_layer(layer).output for layer
-                         in self.style_layers]
-        content_outputs = [vgg.get_layer(layer).output for layer
-                           in self.content_layer]
+            if isinstance(layer, tf.keras.layers.MaxPooling2D):
+                layer = tf.keras.layers.AveragePooling2D(name=layer.name)
+                avg.add(layer)
+            avg.add(layer)
+        style_outputs = [avg.get_layer(layer).get_output_at(1) for
+                         layer in self.style_layers]
+        content_outputs = [avg.get_layer(layer).get_output_at(1) for
+                           layer in self.content_layer]
         model_outputs = style_outputs + content_outputs
         global model
-        model = tf.keras.models.Model(vgg.input, model_outputs)
+        model = tf.keras.models.Model(avg.layers[0].input, model_outputs)
 
+    @staticmethod
     def gram_matrix(input_layer):
         """
         Calculate gram matrices
@@ -66,9 +72,10 @@ class NST:
         if not isinstance(input_layer, tf.Tensor) or \
            isinstance(input_layer, tf.Variable) or \
            len(input_layer.shape) != 4:
-            raise TypeError("input_layer must be a tensor of rank 4")
+                raise TypeError("input_layer must be a tensor of rank 4")
         channels = int(input_layer.shape[-1])
-        a = tf.reshape(input_layer, [-1, channels])
-        n = tf.shape(a)[1]
-        gram = tf.matmul(a, a, transpose_a=True)
-        return gram / tf.cast(n, tf.float32)
+        a = tf.reshape(input_layer, shape=[-1, channels])
+        n = tf.shape(a)[0]
+        gram = tf.matmul(tf.transpose(a), a) / tf.cast(n, tf.float32)
+        gram = tf.reshape(gram, shape=[1, -1, channels])
+        return gram
