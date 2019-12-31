549a8802d6527ede6fc45e89f699b60d86f4fec9
KatyaKalache <katya@kalache.fr>
2019-12-31 00:48:48 +0000

    Changing Max pooling to Average

--- supervised_learning/0x0C-neural_style_transfer/1-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/1-neural_style.py
@@ -11,8 +11,8 @@ class NST:
     """
     Class NST
     """
-    style_layers = ['block1_conv1', 'block2_conv1',
-                    'block3_conv1', 'block4_conv1', 'block5_conv1']
+    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
+                    'block4_conv1', 'block5_conv1']
     content_layer = ['block5_conv2']
 
     def __init__(self, style_image, content_image, alpha=1e1, beta=1e3):
@@ -48,13 +48,18 @@ class NST:
         """
         Creates the model used to calculate cost
         """
-        vgg = tf.keras.applications.vgg19.VGG19()
+        vgg = tf.keras.applications.vgg19.VGG19(include_top=False)
+        avg = tf.keras.Sequential()
         for layer in vgg.layers:
             layer.trainable = False
-        style_outputs = [vgg.get_layer(layer).output for layer
-                         in self.style_layers]
-        content_outputs = [vgg.get_layer(layer).output for layer
-                           in self.content_layer]
+            if isinstance(layer, tf.keras.layers.MaxPooling2D):
+                layer = tf.keras.layers.AveragePooling2D(name=layer.name)
+                avg.add(layer)
+            avg.add(layer)
+        style_outputs = [avg.get_layer(layer).get_output_at(1)
+                         for layer in self.style_layers]
+        content_outputs = [avg.get_layer(layer).get_output_at(1)
+                           for layer in self.content_layer]
         model_outputs = style_outputs + content_outputs
         global model
-        model = tf.keras.models.Model(vgg.input, model_outputs)
+        model = tf.keras.models.Model(avg.layers[0].input, model_outputs)
