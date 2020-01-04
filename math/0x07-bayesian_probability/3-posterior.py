c9e66d7db63ada04248020b9a2865a5b11f816db
KatyaKalache <katya@kalache.fr>
2020-01-04 02:11:06 +0000

    Tasks for Bayesian probability project

---
+++ math/0x07-bayesian_probability/3-posterior.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Calculates the intersection of obtaining this data 
+"""
+from math import factorial
+
+
+def likelihood(x, n, P):
+    """
+    Returns: a 1D numpy.ndarray containing the likelihood
+    of obtaining the data, x and n, for each probability in P
+    """
+    res = []
+    if P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if 0 < P.any() > 1:
+        raise ValueError("All values in P must be in the range [0, 1]")
+    for theta in P:
+        res.append((factorial(n) /(factorial(x) * factorial(n - x)))* (theta ** x) * ((1 - theta) ** (n - x)))
+    return res
+
+def intersection(x, n, P, Pr):
+    """
+    Returns: a 1D numpy.ndarray containing the intersection
+    """
+    res = likelihood(x,n,P) * Pr
+    return res
+
+def marginal(x, n, P, Pr):
+    """
+    Returns: the marginal probability of obtaining x and n
+    """
+    inter = intersection(x, n, P, Pr)
+    return sum(inter)
+
+def posterior(x, n, P, Pr):
+    """
+    Returns: the posterior probability of
+    each probability in P given x and n
+    """
+    marg = marginal(x, n, P, Pr)
+    inter = intersection(x, n, P, Pr)
+    res = inter / marg
+    return res
