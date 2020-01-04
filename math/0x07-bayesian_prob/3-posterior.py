8612a99cb909b87f2b1cf289b7ec9dbda964b222
KatyaKalache <katya@kalache.fr>
2020-01-04 02:30:31 +0000

    pycodestyle fix

--- math/0x07-bayesian_prob/3-posterior.py
+++ math/0x07-bayesian_prob/3-posterior.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Calculates the intersection of obtaining this data 
+Calculates the intersection of obtaining this data
 """
 from math import factorial
 
@@ -16,15 +16,20 @@ def likelihood(x, n, P):
     if 0 < P.any() > 1:
         raise ValueError("All values in P must be in the range [0, 1]")
     for theta in P:
-        res.append((factorial(n) /(factorial(x) * factorial(n - x)))* (theta ** x) * ((1 - theta) ** (n - x)))
+        res.append((factorial(n) / (factorial(x)
+                   * factorial(n - x)))
+                   * (theta ** x) * ((1 - theta)
+                   ** (n - x)))
     return res
+
 
 def intersection(x, n, P, Pr):
     """
     Returns: a 1D numpy.ndarray containing the intersection
     """
-    res = likelihood(x,n,P) * Pr
+    res = likelihood(x, n, P) * Pr
     return res
+
 
 def marginal(x, n, P, Pr):
     """
@@ -32,6 +37,7 @@ def marginal(x, n, P, Pr):
     """
     inter = intersection(x, n, P, Pr)
     return sum(inter)
+
 
 def posterior(x, n, P, Pr):
     """
