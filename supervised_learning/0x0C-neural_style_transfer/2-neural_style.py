e73679957d204ef5c63ec4094ec2261014709174
KatyaKalache <katya@kalache.fr>
2020-01-08 21:52:44 +0000

    Updated init and scale images methods

--- supervised_learning/0x0C-neural_style_transfer/2-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/2-neural_style.py
@@ -15,14 +15,28 @@ class NST:
                     'block4_conv1', 'block5_conv1']
     content_layer = ['block5_conv2']
 
-    def __init__(self, style_image, content_image, alpha=1e1, beta=1e3):
+    def __init__(self, style_image, content_image, alpha=1e4, beta=1):
         """
         Class constructor
         """
+        if not isinstance(style_image, np.ndarray) or \
+           len(style_image.shape) != 3 or \
+           style_image.shape[2] != 3:
+            raise TypeError(
+                "style_image must be a numpy.ndarray with shape (h, w, 3)")
+        if not isinstance(content_image, np.ndarray) or \
+           len(content_image.shape) != 3 or \
+           content_image.shape[2] != 3:
+            raise TypeError(
+                "content_image must be a numpy.ndarray with shape (h, w, 3)")
+        if not isinstance(alpha, (int, float)) or alpha < 0:
+            raise TypeError("alpha must be a non-negative number")
+        if not isinstance(beta, (int, float)) or beta < 0:
+            raise TypeError("beta must be a non-negative number")
         self.style_image = self.scale_image(style_image)
         self.content_image = self.scale_image(content_image)
-        self.alpha = 10000
-        self.beta = 1
+        self.alpha = alpha
+        self.beta = beta
         self.load_model()
         self.model = model
 
@@ -33,16 +47,19 @@ class NST:
         and its largest side is 512 pixels
         Returns the scaled image
         """
+        if not isinstance(image, np.ndarray) or \
+           len(image.shape) != 3 or \
+           image.shape[2] != 3:
+            raise TypeError(
+                "image must be a numpy.ndarray with shape (h, w, 3)")
         height = image.shape[0]
         width = image.shape[1]
-        new_width = 512
-        new_height = int(new_width * height / width)
-        image_scaled = tf.image.resize_images(
-            image, (new_height, new_width),
-            method=tf.image.ResizeMethod.BICUBIC)
-        image_scaled = tf.reshape(image_scaled, (1, new_height, new_width, 3))
-        norm_img = (image_scaled - np.min(image_scaled))/np.ptp(image_scaled)
-        return norm_img
+        scale = 512 / max(height, width)
+        new_shape = (int(scale * height), int(scale * width))
+        image = np.expand_dims(image, axis=0)
+        image_scaled = tf.clip_by_value(tf.image.resize_bicubic
+                                        (image, new_shape) / 255.0, 0.0, 1.0)
+        return image_scaled
 
     def load_model(self):
         """
@@ -56,10 +73,10 @@ class NST:
                 layer = tf.keras.layers.AveragePooling2D(name=layer.name)
                 avg.add(layer)
             avg.add(layer)
-        style_outputs = [avg.get_layer(layer).get_output_at(1) for
-                         layer in self.style_layers]
-        content_outputs = [avg.get_layer(layer).get_output_at(1) for
-                           layer in self.content_layer]
+        style_outputs = [avg.get_layer(layer).get_output_at(1)
+                         for layer in self.style_layers]
+        content_outputs = [avg.get_layer(layer).get_output_at(1)
+                           for layer in self.content_layer]
         model_outputs = style_outputs + content_outputs
         global model
         model = tf.keras.models.Model(avg.layers[0].input, model_outputs)
@@ -69,10 +86,9 @@ class NST:
         """
         Calculate gram matrices
         """
-        if not isinstance(input_layer, tf.Tensor) or \
-           isinstance(input_layer, tf.Variable) or \
-           len(input_layer.shape) != 4:
-                raise TypeError("input_layer must be a tensor of rank 4")
+        if not isinstance(input_layer, (tf.Tensor, tf.Variable)) or len(
+                input_layer.shape) != 4:
+            raise TypeError("input_layer must be a tensor of rank 4")
         channels = int(input_layer.shape[-1])
         a = tf.reshape(input_layer, shape=[-1, channels])
         n = tf.shape(a)[0]
