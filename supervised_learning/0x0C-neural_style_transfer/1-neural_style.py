6cfb7019ba316d76ba5c223eadbd6be6bd73a9f5
KatyaKalache <katya@kalache.fr>
2020-01-08 07:44:15 +0000

    Updating 1-neural based on all fixes from 0-neural

--- supervised_learning/0x0C-neural_style_transfer/1-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/1-neural_style.py
@@ -13,18 +13,30 @@ class NST:
     """
     style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
                     'block4_conv1', 'block5_conv1']
-    content_layer = ['block5_conv2']
+    content_layer = 'block5_conv2'
 
-    def __init__(self, style_image, content_image, alpha=1e1, beta=1e3):
+    def __init__(self, style_image, content_image, alpha=1e4, beta=1):
         """
         Class constructor
         """
+        if not isinstance(style_image, np.ndarray) or \
+           len(style_image.shape) != 3 or \
+           style_image.shape[2] != 3:
+            raise TypeError(
+                "style_image must be a numpy.ndarray with shape (h, w, 3)")
+        if not isinstance(content_image, np.ndarray) or \
+           len(content_image.shape) != 3 or \
+           content_image.shape[2] != 3:
+            raise TypeError(
+                "content_image must be a numpy.ndarray with shape (h, w, 3)")
+        if not isinstance(alpha, (int, float)) or alpha < 0:
+            raise TypeError("alpha must be a non-negative number")
+        if not isinstance(beta, (int, float)) or beta < 0:
+            raise TypeError("beta must be a non-negative number")
         self.style_image = self.scale_image(style_image)
         self.content_image = self.scale_image(content_image)
-        self.alpha = 10000
-        self.beta = 1
-        self.load_model()
-        self.model = model
+        self.alpha = alpha
+        self.beta = beta
 
     @staticmethod
     def scale_image(image):
@@ -33,16 +45,19 @@ class NST:
         and its largest side is 512 pixels
         Returns the scaled image
         """
+        if not isinstance(image, np.ndarray) or \
+           len(image.shape) != 3 or \
+           image.shape[2] != 3:
+            raise TypeError(
+                "image must be a numpy.ndarray with shape (h, w, 3)")
         height = image.shape[0]
         width = image.shape[1]
-        new_width = 512
-        new_height = int(new_width * height / width)
-        image_scaled = tf.image.resize_images(
-            image, (new_height, new_width),
-            method=tf.image.ResizeMethod.BICUBIC)
-        image_scaled = tf.reshape(image_scaled, (1, new_height, new_width, 3))
-        norm_img = (image_scaled - np.min(image_scaled))/np.ptp(image_scaled)
-        return norm_img
+        scale = 512 / max(height, width)
+        new_shape = (int(scale * height), int(scale * width))
+        image = np.expand_dims(image, axis=0)
+        image_scaled = tf.clip_by_value(tf.image.resize_bicubic
+                                        (image, new_shape) / 255.0, 0.0, 1.0)
+        return image_scaled
 
     def load_model(self):
         """
